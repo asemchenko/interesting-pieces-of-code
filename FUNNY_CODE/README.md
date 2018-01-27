6e07cd6c972a5f47835266539ac233c755b84b9c
Asem <andriysemchenko@gmail.com>
2018-01-28 01:04:12 +0200

    added FUNNY_CODE

---
+++ FUNNY_CODE/README.md
@@ -0,0 +1,2 @@
+# This is collection of code that looks like just a set of random chars
+## But this code really valid(and can be compiled, executed) in C++ or other language
---
+++ FUNNY_CODE/main.cpp
@@ -0,0 +1,11 @@
+void lol(void);
+void kek(void);
+void ololo(void);
+void lo(void);
+int cheburek;
+int main() {
+	lol,kek,ololo,lo,lo,lo;
+	for(0;0;0) {{{{{{{}}}}}}}
+	lol;kek;cheburek;;;;;;;;;;;;;;;;;;;;;;
+	
+}
